44417a8fa604f4af11e5273c708d79022388c36f
David E. Box <david.e.box@linux.intel.com>
2024-02-23 14:58:50 -0600

    BACKPORT: PCI/ASPM: Save L1 PM Substates Capability for suspend/resume
    4ff116d0d5fd ("PCI/ASPM: Save L1 PM Substates Capability for
    suspend/resume") restored the L1 PM Substates Capability after resume,
    which reduced power consumption by making the ASPM L1.x states work after
    resume.
    
    a7152be79b62 ("Revert "PCI/ASPM: Save L1 PM Substates Capability for
    suspend/resume"") reverted 4ff116d0d5fd because resume failed on some
    systems, so power consumption after resume increased again.
    
    a7152be79b62 mentioned that we restore L1 PM substate configuration even
    though ASPM L1 may already be enabled. This is due the fact that the
    pci_restore_aspm_l1ss_state() was called before pci_restore_pcie_state().
    
    Save and restore the L1 PM Substates Capability, following PCIe r6.1, sec
    5.5.4 more closely by:
    
      1) Do not restore ASPM configuration in pci_restore_pcie_state() but
         do that after PCIe capability is restored in pci_restore_aspm_state()
         following PCIe r6.1, sec 5.5.4.
    
      2) If BIOS reenables L1SS, particularly L1.2, we need to clear the
         enables in the right order, downstream before upstream. Defer
         restoring the L1SS config until we are at the downstream component.
         Then update the config for both ends of the link in the prescribed
         order.
    
      3) Program ASPM L1 PM substate configuration before L1 enables.
    
      4) Program ASPM L1 PM substate enables last, after rest of the fields
         in the capability are programmed.
    
    [bhelgaas: commit log, squash L1SS-related patches, do both LNKCTL restores
    in pci_restore_pcie_state()]
    
    Link: https://lore.kernel.org/r/20240128233212.1139663-3-david.e.box@linux.intel.com
    Link: https://lore.kernel.org/r/20240128233212.1139663-4-david.e.box@linux.intel.com
    Link: https://lore.kernel.org/r/20240223205851.114931-5-helgaas@kernel.org
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=217321
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=216782
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=216877
    Co-developed-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Co-developed-by: David E. Box <david.e.box@linux.intel.com>
    Reported-by: Koba Ko <koba.ko@canonical.com>
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: David E. Box <david.e.box@linux.intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Tested-by: Tasev Nikola <tasev.stefanoska@skynet.be> # Asus UX305FA
    Cc: Mark Enriquez <enriquezmark36@gmail.com>
    Cc: Thomas Witt <kernel@witt.link>
    Cc: Werner Sembach <wse@tuxedocomputers.com>
    Cc: Vidya Sagar <vidyas@nvidia.com>
    
    BUG: 371115070
    Change-Id: I76df8718c3b88935d14be52e848ea9db8145e171
    (cherry picked from commit 17423360a27ae58c1850f588bdd8013bbfcd250b)
    [sushrut:
    a) Used pci_clear_and_set_dword() instead of
       pci_clear_and_set_config_dword() in pci_restore_aspm_l1ss_state()
       since the latter doesn't exist on 5.15.
    b) Also moved pci_clear_and_set_dword towards the beginning.
    c) In contrast to upstream, we are enabling save/restore only when
       CONFIG_PCIEASPM is enabled to avoid making changes in pci_dev
       struture. Hence, added required inline declarations when
       CONFIG_PCIEASPM is not set]
    Signed-off-by: Sushrut Shree Trivedi <quic_sushruts@quicinc.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -606,6 +606,9 @@ void pcie_aspm_powersave_config_link(struct pci_dev *pdev);
 void pci_configure_ltr(struct pci_dev *pdev);
 void pci_save_ltr_state(struct pci_dev *dev);
 void pci_restore_ltr_state(struct pci_dev *dev);
+void pci_configure_aspm_l1ss(struct pci_dev *dev);
+void pci_save_aspm_l1ss_state(struct pci_dev *dev);
+void pci_restore_aspm_l1ss_state(struct pci_dev *dev);
 #else
 static inline void pcie_aspm_init_link_state(struct pci_dev *pdev) { }
 static inline void pcie_aspm_exit_link_state(struct pci_dev *pdev) { }
@@ -614,6 +617,9 @@ static inline void pcie_aspm_powersave_config_link(struct pci_dev *pdev) { }
 static inline void pci_configure_ltr(struct pci_dev *pdev) { }
 static inline void pci_save_ltr_state(struct pci_dev *dev); { }
 static inline void pci_restore_ltr_state(struct pci_dev *dev); { }
+static inline void pci_configure_aspm_l1ss(struct pci_dev *dev); { }
+static inline void pci_save_aspm_l1ss_state(struct pci_dev *dev); { }
+static inline void pci_restore_aspm_l1ss_state(struct pci_dev *dev); { }
 #endif
 
 #ifdef CONFIG_PCIE_ECRC
