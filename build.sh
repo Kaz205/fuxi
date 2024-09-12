6a535f4f7c988dd1f1ca88f57048ebf3ad2fc1b9
Kazuki Hashimoto <kazukih0205@gmail.com>
2024-09-12 19:11:04 +0900

    Add build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+export ARCH=arm64
+export SUBARCH=arm64
+export ak=$HOME/AnyKernel3_fuxi
+export PATH="$HOME/llvm/bin:$PATH"
+
+NAME=$(git describe --exact-match --tags 2> /dev/null || git rev-parse --short HEAD)
+MAKE_PARAMS="LLVM=1 LLVM_IAS=1"
+git submodule init && git submodule update
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+make $MAKE_PARAMS fuxi_defconfig -j$(nproc --all)
+make $MAKE_PARAMS Image -j$(nproc --all)
+
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+cp out/arch/arm64/boot/Image $ak/Image
+cd $ak
+zip -FSr9 $NAME.zip ./*  -x "*.zip"
+echo "Kernel zip name: $NAME"
